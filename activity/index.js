33039ed94523e53d5d7673e483035670df8dcb19
Rofiq Ahmad Mubarok <rofiqahmadm@gmail.com>
2023-01-25 11:56:14 +0700

    rename file activity

---
+++ activity/index.js
@@ -0,0 +1,12 @@
+const dayItems = [
+  { name: "sunday", activity: ["watchMovie", "eatFood", "pray", ""] },
+  {
+    name: "monday",
+    activity: ["celanTheRoom", "breakfast", "playingMyLaptop", "pray"],
+  },
+  { name: "tuesday" },
+  { name: "wednesday" },
+  { name: "thursday" },
+  { name: "friday" },
+  { name: "saturday" },
+];
